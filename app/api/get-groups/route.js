0dff54eb8f724958c2da0860e4f1bb934eaaa45d
yudaaditya245 <yudaaditya245@gmail.com>
2023-12-02 07:48:24 +0700

    dev: fix vercel error

--- app/api/get-groups/route.js
+++ app/api/get-groups/route.js
@@ -1,12 +1,12 @@
 import { Scraps } from "@/sequelize/models";
 
 export async function GET(request) {
-  const group = request.nextUrl.searchParams.get("group");
+  // const group = request.nextUrl.searchParams.get("group");
 
   try {
     const req = await Scraps.findAll({
       where: {
-        source: group,
+        // source: group,
         mainId: null
       },
       order: [["updated_at", "DESC"]]
