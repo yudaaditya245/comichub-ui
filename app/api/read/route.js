a5aa5705b803054bf0cff640b23a74307c52f8f7
yudaaditya245 <yudaaditya245@gmail.com>
2024-01-03 16:38:39 +0700

    update prisma schema

--- app/api/read/route.js
+++ app/api/read/route.js
@@ -1,49 +1,49 @@
-// import prisma from "@/lib/prismaClient";
-import { getImages } from "@/lib/fetchChapter";
-import { PrismaClient } from "@prisma/client";
+// // import prisma from "@/lib/prismaClient";
+// import { getImages } from "@/lib/fetchChapter";
+// import { PrismaClient } from "@prisma/client";
 
-import axios from "axios";
-const prisma = new PrismaClient();
+// import axios from "axios";
+// const prisma = new PrismaClient();
 
-export async function GET(request) {
-  const id = parseInt(request.nextUrl.searchParams.get("id"));
+// export async function GET(request) {
+//   const id = parseInt(request.nextUrl.searchParams.get("id"));
 
-  if (!id) {
-    return Response.error("Forbidden");
-  }
+//   if (!id) {
+//     return Response.error("Forbidden");
+//   }
 
-  try {
-    const _getScrap = await prisma.scraps.findUnique({
-      where: {
-        id
-      }
-    });
+//   try {
+//     const _getScrap = await prisma.scraps.findUnique({
+//       where: {
+//         id
+//       }
+//     });
 
-    // if already have images in db, then return images
-    if (_getScrap.images !== null) {
-      return Response.json(JSON.parse(_getScrap.images));
-    }
+//     // if already have images in db, then return images
+//     if (_getScrap.images !== null) {
+//       return Response.json(JSON.parse(_getScrap.images));
+//     }
 
-    const { data } = await axios.get(_getScrap.link_chapter, { timeout: 20000 });
-    const images = getImages(_getScrap.source, data);
+//     const { data } = await axios.get(_getScrap.link_chapter, { timeout: 20000 });
+//     const images = getImages(_getScrap.source, data);
 
-    if (!images) {
-      return Response.error("fetch failed!");
-    }
+//     if (!images) {
+//       return Response.error("fetch failed!");
+//     }
 
-    await prisma.scraps.update({
-      where: {
-        id
-      },
-      data: {
-        images: JSON.stringify(images)
-      }
-    });
+//     await prisma.scraps.update({
+//       where: {
+//         id
+//       },
+//       data: {
+//         images: JSON.stringify(images)
+//       }
+//     });
 
-    return Response.json(images);
-  } catch (error) {
-    return Response.error(error.message);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+//     return Response.json(images);
+//   } catch (error) {
+//     return Response.error(error.message);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
