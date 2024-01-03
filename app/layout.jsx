2f7b229a41a892ec14e576c8794af722d0ae0ddc
yudaaditya245 <yudaaditya245@gmail.com>
2024-01-03 11:55:05 +0700

    downgrade nextjs, add toploading state

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 import { Signika_Negative } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import NextTopLoader from "nextjs-toploader";
 
 const signika_Negative = Signika_Negative({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={twMerge(signika_Negative.className, "bg-light-300")}>
+        <NextTopLoader showSpinner={false}/>
         <Provider>
           <div className="flex min-h-screen flex-col">
             <div className="flex-grow pb-10">{children}</div>
